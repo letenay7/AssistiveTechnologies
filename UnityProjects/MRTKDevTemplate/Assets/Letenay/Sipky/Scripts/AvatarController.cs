686315e52272aa9ac38d36120625ac8fb938e031
letenay7 <99814299+letenay7@users.noreply.github.com>
2025-10-17 20:32:55 +0200

    add  rotation and reverse multipliers

--- UnityProjects/MRTKDevTemplate/Assets/Letenay/Sipky/Scripts/AvatarController.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Letenay/Sipky/Scripts/AvatarController.cs
@@ -13,10 +13,15 @@ public class AvatarController : MonoBehaviour
     private Rigidbody rigidBody;
    
     private float moveDistance = 1f;
-    private float speed = 1;
-    //  TODO add multiplier for turning speed and reverse
+    private float forwardSpeed = 1f;
+    private float reverseMultiplier = 0.5f;
+    private float rotateMultiplier = 0.8f;
+    private bool isMoving = false;
+    private bool isTurning = false;
+    
     // stlacim tlacitko a hybem sa kym neslacim ine  alebo pokial neuhnem pohladom
-    // ked uhnem pohladom tak spravim stop 
+    // TODO refactor method order
+    // TODO Manage movement from menuScript via Statefulinteractable isGazeHovered
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -25,20 +30,24 @@ public class AvatarController : MonoBehaviour
 
     public void IncreaseSpeed() 
     {
-        if (speed < 5) 
+        if (forwardSpeed < 5) 
         {
-            speed++;
-            Debug.Log($"Speed increased. Current speed: {speed}");
+            forwardSpeed++;
+            Debug.Log($"Speed increased. Current speed: {forwardSpeed}");
         }
        
     }
     public void DecreaseSpeed()
     {
-        if (speed > 1) 
+        if (forwardSpeed > 1)
         {
-            speed--;
-            Debug.Log($"Speed decreased. Current speed: {speed}");
+            forwardSpeed--;
+            Debug.Log($"Speed decreased. Current speed: {forwardSpeed}");
         }
+    }
+    public void StartMovement()
+    {
+        isMoving = true;
     }
     public void Forward()
     {
@@ -46,30 +55,69 @@ public class AvatarController : MonoBehaviour
     }
     public void Reverse() 
     {
-
         Move(Vector3.back);
     }
-    public void Left() 
+    public void Left()
     {
-        Move(Vector3.left);
-        /// left a right ma byt iba otacanie danym smerom
+        Turn(Vector3.left);
     }
 
-    public void Right() 
+    public void Right()
     {
-        Move(Vector3.right);
+        Turn(Vector3.right);
     }
 
-    private void Move(Vector3 direction)
+    public void Move(Vector3 direction)
     {
-        Vector3 moveVector = direction.normalized * moveDistance * speed;
-        rigidBody.velocity = moveVector;
+        Debug.Log($"Moving enabled {isMoving}");
+        if (isMoving)
+        {
+            Vector3 moveVector = direction.normalized;
+            if (direction == Vector3.forward)
+            {
+                moveVector *= forwardSpeed;
+            }
+            else
+            {
+                moveVector *= reverseMultiplier * forwardSpeed;
+            }
+            rigidBody.velocity = moveVector;
+        }
     }
-    
+    private void Turn(Vector3 direction)
+    {
+       if (isTurning)
+        {
+            Vector3 rotationVector = Vector3.up * rotateMultiplier;
+            if (direction == Vector3.right)
+            {
+                transform.Rotate(rotationVector);
+            }
+            else
+            {
+                transform.Rotate(-rotationVector);
+            }
+        }
+    }
+
     public void MakeSound()
     {
         Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
+    public void StopMovement()
+    {
+        isMoving = false;
+        rigidBody.velocity = Vector3.zero;
+        Debug.Log("Stop");
+    }
 
-    
+    public void StopTurn()
+    {
+        isTurning = false;
+    }
+
+    public void StartTurn()
+    {
+        isTurning = true;
+    } 
 }
