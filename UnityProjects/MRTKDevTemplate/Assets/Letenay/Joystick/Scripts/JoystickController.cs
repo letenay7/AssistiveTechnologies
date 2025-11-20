77595bf20362f38a44835f15515045dd73d42373
letenay7 <99814299+letenay7@users.noreply.github.com>
2025-11-20 15:34:23 +0100

    add joystick value calclations

--- UnityProjects/MRTKDevTemplate/Assets/Letenay/Joystick/Scripts/JoystickController.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Letenay/Joystick/Scripts/JoystickController.cs
@@ -1,31 +1,31 @@
-using System;
 using UnityEngine;
 
 public class JoystickController : MonoBehaviour
 {
     private bool isWithinBounds;
-    [SerializeField] private GameObject gazeIndicator;
+    [SerializeField]
+    private Transform gazeIndicator;
     private float x;
     private float y;
     private Vector3 defaultIndicatorPosition = Vector3.zero;
 
     void Start()
     {
-        defaultIndicatorPosition = gazeIndicator.transform.position;
+        defaultIndicatorPosition = gazeIndicator.localPosition;
     }
 
     void Update()
     {
         if (isWithinBounds)
         {
-            gazeIndicator.transform.position = new Vector3(x, y, gazeIndicator.transform.position.z);
+            gazeIndicator.position = new Vector3(x, y, gazeIndicator.position.z);
         }
     }
 
     public void DisableMovement()
     {
         isWithinBounds = false;
-        gazeIndicator.transform.position = defaultIndicatorPosition;
+        gazeIndicator.localPosition = defaultIndicatorPosition;
         Debug.Log("Movement Disabled");
     }
 
@@ -41,24 +41,20 @@ public class JoystickController : MonoBehaviour
         Vector3 localPosition = hitObject.InverseTransformPoint(hit.point);
         if (isWithinBounds)
         {
+            Debug.Log(localPosition);
             x = hit.point.x;
             y = hit.point.y;
-            localPosition.z = defaultIndicatorPosition.z;
-            //Debug.Log(Vector3.Distance(localPosition, defaultIndicatorPosition));
-            if (Vector3.Distance(localPosition, defaultIndicatorPosition) > 150f)
+            /* Values accepted by the wheelchair are 0-255
+               Our joystick pointer is at local coords (0, 0, 0) and the joystick has size 260x260
+               considering the size of the gaze pointer and tolerance local values on the joystick are <-128, 128>
+            */
+            if (Mathf.Abs(localPosition.x) > 128f || Mathf.Abs(localPosition.y) > 128f)
             {
                 DisableMovement();
             }
-
-            Tuple<float, float> joystickValues = CalculateJoystickValues(localPosition.x, localPosition.y);
-            //Debug.Log($"x: {joystickValues.Item1}, y: {joystickValues.Item2}");
+            int joystickX = Mathf.RoundToInt(localPosition.x) + 128;
+            int joystickY = Mathf.RoundToInt(localPosition.y)  + 128;
+            Debug.Log($"x:{joystickX} y:{joystickY}");
         }
-    }
-
-    private Tuple<float, float> CalculateJoystickValues(float localX, float localY)
-    {
-        localX += 128f;
-        localY += 128f;
-        return new Tuple<float, float>(localX, localY);
     }
 }
